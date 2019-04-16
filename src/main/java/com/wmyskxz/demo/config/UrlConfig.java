756742bfc5ae2731375d916ee623ac0eadce4a38
wmyskxz <939419061@qq.com>
2019-04-16 21:13:42 +0800

    增加友好的注释

--- src/main/java/com/wmyskxz/demo/config/UrlConfig.java
+++ src/main/java/com/wmyskxz/demo/config/UrlConfig.java
@@ -7,6 +7,8 @@ package com.wmyskxz.demo.config;
  * @date:2019/04/16 - 16:29
  */
 public class UrlConfig {
+    /** 后台基础地址 */
     public static final String BASE_URL = "http://127.0.0.1/";
+    /** 短链接基础地址,后跟shortCode */
     public static final String BASE_SHORT_URL = BASE_URL + "r/";
 }
